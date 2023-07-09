7afc07c59aaf8c23ec80c1524ec2258cc2c6a73c
4nyNoob <aniziooliveira9@gmail.com>
2023-07-09 19:14:43 -0300

    Initial commit

---
+++ src/main/java/any/brazilasthetic/BrazilAsthetic.java
@@ -0,0 +1,17 @@
+package any.brazilasthetic;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BrazilAsthetic implements ModInitializer {
+
+	public static final String MOD_ID = "brazil-asthetic";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/any/brazilasthetic/BrazilAstheticClient.java
@@ -0,0 +1,10 @@
+package any.brazilasthetic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BrazilAstheticClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
