e6ea864c58b7903d26ea3f8eaf2d552c08613a96
rafaelZapico <rafaelzapico00@gmail.com>
2023-09-17 14:09:44 -0300

    updating food recipes

--- src/main/java/any/brazilaesthetic/datagen/ModRecipeProvider.java
+++ src/main/java/any/brazilaesthetic/datagen/ModRecipeProvider.java
@@ -39,7 +39,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         //SHAPED RECIPE
         ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.AMBER_PLATE, 3)
                 //.pattern("ooo")
-                .pattern("# #")
+//                .pattern("# #")
                 .pattern("###")
                 .input('#' , Items.BROWN_STAINED_GLASS)
                 .criterion(hasItem(Items.BROWN_STAINED_GLASS), FabricRecipeProvider.conditionsFromItem(Items.BROWN_STAINED_GLASS))
@@ -53,5 +53,80 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .input('X' , Items.COOKED_CHICKEN)
                 .criterion(hasItem(Items.WHEAT), FabricRecipeProvider.conditionsFromItem(Items.WHEAT))
                 .offerTo(exporter, new Identifier(getRecipeName(ModItems.COXINHA)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.CUP_AMERICAN, 3)
+                .pattern("# #")
+                .pattern(" # ")
+                .input('#' , Items.GLASS)
+                .criterion(hasItem(Items.GLASS), FabricRecipeProvider.conditionsFromItem(Items.GLASS))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.CUP_AMERICAN)));
+
+
+        //sucos
+        ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.ACEROLA_JUICE, 1)
+                .pattern("X#")
+                .input('#' , ModItems.CUP_AMERICAN)
+                .input('X' , ModItems.ACEROLA)
+                .criterion(hasItem(ModItems.CUP_AMERICAN), FabricRecipeProvider.conditionsFromItem(ModItems.CUP_AMERICAN))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.ACEROLA_JUICE)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.AVOCADO_JUICE, 1)
+                .pattern("X#")
+                .input('#' , ModItems.MILK_CUP_AMERICAN)
+                .input('X' , ModItems.AVOCADO)
+                .criterion(hasItem(ModItems.MILK_CUP_AMERICAN), FabricRecipeProvider.conditionsFromItem(ModItems.MILK_CUP_AMERICAN))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.AVOCADO_JUICE)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.GUARANA_JUICE, 1)
+                .pattern("X#")
+                .input('#' , ModItems.CUP_AMERICAN)
+                .input('X' , ModItems.GUARANA)
+                .criterion(hasItem(ModItems.CUP_AMERICAN), FabricRecipeProvider.conditionsFromItem(ModItems.CUP_AMERICAN))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.GUARANA_JUICE)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.ORANGE_JUICE, 1)
+                .pattern("X#")
+                .input('#' , ModItems.CUP_AMERICAN)
+                .input('X' , ModItems.ORANGE)
+                .criterion(hasItem(ModItems.CUP_AMERICAN), FabricRecipeProvider.conditionsFromItem(ModItems.CUP_AMERICAN))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.ORANGE_JUICE)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.CANE_JUICE, 1)
+                .pattern("X#")
+                .input('#' , ModItems.CUP_AMERICAN)
+                .input('X' , Items.SUGAR_CANE)
+                .criterion(hasItem(ModItems.CUP_AMERICAN), FabricRecipeProvider.conditionsFromItem(ModItems.CUP_AMERICAN))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.CANE_JUICE)));
+
+        //chas
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.CIDER_HERB_TEA, 2)
+                .pattern("#X#")
+                .pattern(" S ")
+                .input('#' , ModItems.CIDER_HERB)
+                .input('S' , ModBlocks.AMBER_MUG)
+                .input('X' , Items.WATER_BUCKET)
+                .criterion(hasItem(Items.WATER_BUCKET), FabricRecipeProvider.conditionsFromItem(Items.WATER_BUCKET))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.CIDER_HERB_TEA)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.FENNEL_TEA, 2)
+                .pattern("#X#")
+                .pattern(" S ")
+                .input('#' , ModItems.FENNEL)
+                .input('S' , ModBlocks.AMBER_MUG)
+                .input('X' , Items.WATER_BUCKET)
+                .criterion(hasItem(Items.WATER_BUCKET), FabricRecipeProvider.conditionsFromItem(Items.WATER_BUCKET))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.FENNEL_TEA)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.YERBA_MATE_TEA, 2)
+                .pattern("#X#")
+                .pattern(" S ")
+                .input('#' , ModItems.YERBA_MATE)
+                .input('S' , ModBlocks.AMBER_MUG)
+                .input('X' , Items.WATER_BUCKET)
+                .criterion(hasItem(Items.WATER_BUCKET), FabricRecipeProvider.conditionsFromItem(Items.WATER_BUCKET))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.YERBA_MATE_TEA)));
+
     }
 }
+
