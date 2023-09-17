e4bf616944867a6da46fc30e70bd2cc97a161405
4nyNoob <aniziooliveira9@gmail.com>
2023-09-17 13:54:57 -0300

    added recipe templates

--- src/main/java/any/brazilaesthetic/datagen/ModRecipeProvider.java
+++ src/main/java/any/brazilaesthetic/datagen/ModRecipeProvider.java
@@ -6,18 +6,22 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.util.Identifier;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
-    /*
-    private static final List<ItemConvertible>  PLACEHOLDER_SMELTABLES = List.of(
-            ModItems.CHEESE, ModItems.CHEESE_BREAD
+
+    //SMELTABLE INGREDIENTS LIST
+    private static final List<ItemConvertible>  BEIJU_SMELTABLES = List.of(
+            ModItems.MANDIOCA_FLOUR
     );
-    */
+
 
     public ModRecipeProvider(FabricDataOutput output) {
         super(output);
@@ -25,9 +29,10 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public void generate(Consumer<RecipeJsonProvider> exporter) {
+
+        offerSmelting(exporter, BEIJU_SMELTABLES, RecipeCategory.MISC, ModItems.BEIJU,
+                0.7f, 200, "brazil-aesthetic");
         /*
-        offerSmelting(exporter, PLACEHOLDER_SMELTABLES, RecipeCategory.MISC, ModItems.MANDIOCA,
-                0.7f, 200, "placeholder");
         offerBlasting(exporter, PLACEHOLDER_SMELTABLES, RecipeCategory.MISC, ModItems.MANDIOCA,
                 0.7f, 100, "placeholder");
         */
@@ -37,21 +42,34 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 RecipeCategory.DECORATIONS, ModBlocks.COPACABANA_SIDEWALK);
 
         //SHAPED RECIPE
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.AMBER_PLATE, 3)
-                //.pattern("ooo")
-                .pattern("# #")
-                .pattern("###")
-                .input('#' , Items.BROWN_STAINED_GLASS)
-                .criterion(hasItem(Items.BROWN_STAINED_GLASS), FabricRecipeProvider.conditionsFromItem(Items.BROWN_STAINED_GLASS))
-                .offerTo(exporter, new Identifier(getRecipeName(ModBlocks.AMBER_PLATE)));
+            //AMBER PLATE
+            ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.AMBER_PLATE, 3)
+                    //.pattern("ooo")
+                    .pattern("# #")
+                    .pattern("###")
+                    .input('#' , Items.BROWN_STAINED_GLASS)
+                    .criterion(hasItem(Items.BROWN_STAINED_GLASS), FabricRecipeProvider.conditionsFromItem(Items.BROWN_STAINED_GLASS))
+                    .offerTo(exporter, new Identifier(getRecipeName(ModBlocks.AMBER_PLATE)));
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.COXINHA, 4)
-                .pattern(" # ")
-                .pattern("#X#")
-                .pattern("###")
-                .input('#' , Items.WHEAT)
-                .input('X' , Items.COOKED_CHICKEN)
-                .criterion(hasItem(Items.WHEAT), FabricRecipeProvider.conditionsFromItem(Items.WHEAT))
-                .offerTo(exporter, new Identifier(getRecipeName(ModItems.COXINHA)));
+            //COXINHA
+            ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.COXINHA, 4)
+                    .pattern(" # ")
+                    .pattern("#X#")
+                    .pattern("###")
+                    .input('#' , Items.WHEAT)
+                    .input('X' , Items.COOKED_CHICKEN)
+                    .criterion(hasItem(Items.WHEAT), FabricRecipeProvider.conditionsFromItem(Items.WHEAT))
+                    .offerTo(exporter, new Identifier(getRecipeName(ModItems.COXINHA)));
+
+        //SEMI-SHAPELESS RECIPE
+            //ACEROLA JUICE
+            ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.ACEROLA_JUICE, 1)
+                    .pattern("#X")
+                    .input('#' , ModItems.CUP_AMERICAN)
+                    .input('X' , ModItems.ACEROLA)
+                    .criterion(hasItem(ModItems.CUP_AMERICAN), conditionsFromItem(ModItems.CUP_AMERICAN))
+                    .criterion(hasItem(ModItems.ACEROLA), conditionsFromItem(ModItems.ACEROLA_JUICE))
+                    .offerTo(exporter, new Identifier(getRecipeName(ModItems.ACEROLA_JUICE)));
+
     }
 }
